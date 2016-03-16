1e91ddee90f91962567bc08a1b66074e6c1d3293
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-16 10:11:26 -0700

    Added 'for' loop to print bio to the page after initial alert.

--- 16-mar-lab/scripts/scripts.js
+++ 16-mar-lab/scripts/scripts.js
@@ -1,24 +1,34 @@
 alert('Hey, pal. It\'s Taylor. How well do you know me? Let\'s find out! Click OK to read a little bit about me. After that you can play a little Taylor trivia game.');
 
-function printBio(heading, par) {
-  var sectHeading = heading.toUpperCase();
-  document.getElementById(heading).textContent = sectHeading; //Why console.log error saying "can't set textContent of null"?
-  document.getElementById(par).textContent = parText;
+var parText = [
+'Taylor was born on October 20, 1986, in Winston-Salem, North Carolina. She was Cindy and Scott Shirley\'s second daughter, though the adoption of an older sister would eventually make her the third of four daughters. As a child she learned to play the guitar, took dance classes, sang in the church choir, and almost lost a toe to eczema.',
+
+'Taylor studied at UNC-Chapel Hill and graduated in 2009 with a BA in Linguistics and a minor in Hispanic Studies. She spent her penultimate semester studying in Lima, Peru.',
+
+'Taylor has worked as a Spanish interpreter and a massage therapist and currently lives in Portland, Oregon, with her wife Evelyn. She is now learning coding in the hopes of launching a startup with Evelyn, where they will attempt to use their respective skills to take over the world.'
+];
+
+for (var ii=0; ii<parText.length; ii++) {
+  var bioText = parText[0];
+  var elementId = ['childhood', 'bioChild', 'college', 'bioCollege', 'adulthood', 'bioAdult'];
+  for (jj=0; jj<elementId.length; jj++) {
+    var htmlElement = document.getElementById(elementId[jj]);
+    if (jj%2 === 0) {
+      htmlElement.textContent = elementId[jj].toUpperCase()
+    } else {
+      switch (jj) {   //COULD COME UP WITH MATHEMATICAL FUNCTION TO RELATE jj TO ii
+        case 1:
+          htmlElement.textContent = parText[0];
+          break;
+        case 3:
+          htmlElement.textContent = parText[1];
+          break;
+        case 5:
+          htmlElement.textContent = parText[2];
+          break;
+      }
+    }
+  }
 }
 
-/*ABOVE FUNCTION IS AN ATTEMPT TO GET AROUND THIS REPETITIVE CODE
-
-var elChildhood = Document.getElementById("childhood");
-var elBioChild = Document.getElementById("bioChild");
-var elCollege = Document.getElementById("college");
-var elBioCollege = Document.getElementById("bioCollege");
-var elAdulthood = Document.getElementById("adulthood");
-var elBioAdult = Document.getElementById("bioAdult");
-*/
-
-var parText = 'Taylor was born on October 20, 1986, in Winston-Salem, North Carolina. She was Cindy and Scott Shirley\'s second daughter, though the adoption of an older sister ';
-printBio('childhood', 'bioChild');
-var parText = 'Taylor studied at UNC-Chapel Hill and graduated in 2009 with a BA in Linguistics.';
-printBio('college', 'bioCollege');
-var parText = 'Taylor has worked as a Spanish interpreter and a massage therapist and currently lives in Portland, Oregon, with her wife Evelyn.';
-printBio('adulthood', 'bioAdult');
+alert('Peruse my bio, and then click OK when you\'re ready to test how well you know me.')
