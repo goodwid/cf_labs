dc674fcdb15e28853f09ebdd83e8c444171a7dcd
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-18 00:15:07 -0700

    Condensed cases for finalMessage

--- week1-project/scripts/scripts.js
+++ week1-project/scripts/scripts.js
@@ -7,7 +7,8 @@ var userName = prompt('What is your name?');
 
 alert('Hi, ' + userName + '. It\'s Taylor. I\'d like to introduce myself. Please feel free to peruse this page, and then click OK when you\'re ready to play a trivia game about me. Warning: The questions have nothing to do with this bio.')
 
-var quizQuestions = [   //Array with objects that each represent a question-answer pair
+//Array with objects that each represent a question-answer pair
+var quizQuestions = [
   {
     'question': 'For what television competition show did I audition, make it through the first round, and then go on to the second round only to embarrass myself in a brilliant supernova of nerve-driven failure?',
     'answer': 'AMERICAN IDOL'
@@ -69,29 +70,37 @@ function askForNumber (quizQuestion, quizAnswer) {
   if (!numberCorrect) {
     alert('Bummer. The answer was actually ' + quizAnswer + '.');
   }
-  console.log('When asked, "' + quizQuestions[kk].question + '" user answered "' + userResponse + '."');
+  console.log('When asked, "' + quizQuestion + '" user answered "' + userResponse + '."');
 }
 
 function askForArray (quizQuestion, quizAnswer) {
   questionCounter++;
   var userResponse = (prompt(quizQuestion)).toUpperCase();
-  console.log('When asked, "' + quizQuestions[kk].question + '" user answered "' + userResponse + '."');
+  console.log('When asked, "' + quizQuestion + '" user answered "' + userResponse + '."');
   if (quizAnswer.indexOf(userResponse)>=0) {
     alert('That\'s correct!');
     correct++;
   } else {
-    var arrayItems = correctResponse.join(', ');
+    var arrayItems = quizAnswer.join(', ');
     alert('Sorry, that\'s incorrect. Any of the following would have been correct: ' + arrayItems + '. Nice try though!');
     }
 }
 
-//call functions to ask questions and analyze answer
+//call functions to ask questions and analyze answer (could replace with for loop that checks for typeOf() quizQuestions[x].answer)
 for (var ii=0; ii<4; ii++) {
-  
+  var ques = quizQuestions[ii].question;
+  var answ = quizQuestions[ii].answer;
+  askForString(ques, answ);
 }
 
+askForNumber(quizQuestions[4].question, quizQuestions[4].answer);
+
+askForArray(quizQuestions[5].question, quizQuestions[5].answer);
+
+//console.log total number of correct answers
 console.log('Questions answered correctly: ' + correct + '.');
 
+//compose final message for user with total correct answers
 var finalMessage = 'You got ' + correct + ' out of ' + questionCounter + ' questions right, ' + userName + '. ';
 
 switch (correct) {    //Depending on number correct, writes some message to the page.
@@ -99,15 +108,11 @@ switch (correct) {    //Depending on number correct, writes some message to the 
     finalMessage += 'Not great, but those questions were tough. I still think you\'re tops.';
     break;
   case 1:
-    finalMessage += 'That\'s actually not bad. Those questions were tough!';
-    break;
   case 2:
-    finalMessage += 'That\'s pretty good. Those questions were not easy!';
-    break;
   case 3:
-  case 4:
-    finalMessage += 'That\'s great! I\'m impressed.';
+    finalMessage += 'That\'s not bad. Those questions were not easy!';
     break;
+  case 4:
   case 5:
     finalMessage += 'Holy crap. Did you cheat?? J/K. Awesome job! Want to be best friends?';
     break;
